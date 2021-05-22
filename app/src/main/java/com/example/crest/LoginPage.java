216d7d6abfe319ec61218a57f62f9aa58e56e45d
mihirpenugonda <mihirpenugonda@gmail.com>
2021-05-22 23:54:03 +0530

    Login Page (Partial)
    Login Page UI Completed TaskBar Implementation Left

---
+++ app/src/main/java/com/example/crest/LoginPage.java
@@ -0,0 +1,37 @@
+package com.example.crest;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+
+public class LoginPage extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_login_page);
+
+        };
+    }
+
+
